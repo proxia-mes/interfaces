117a8254ae3883083c54dc151a5339739746b6a0
FBuchner <fbuchner@proxia.com>
2021-11-17 16:34:31 +0100

    Add topic database-trs_res-change

---
+++ topics/database-trs_res-change/v1/local-gen/GenerateNetClient.cmd
@@ -0,0 +1,12 @@
+REM https://github.com/OpenAPITools/openapi-generator
+
+SET OUTPUT_DIRECTORY=${PWD}/out
+SET INTERFACE_DIRECTORY=${PWD}/..
+SET INTERFACE_FILE=proxia-mes_database-trs_res-change-event_v1.yaml
+SET PACKAGE_NAME=PAG.Mes.MonitoringDashboard.DataBaseTrsResChangeEvent
+SET INTERFACE_VERSION=1.0.0
+
+powershell Remove-Item %OUTPUT_DIRECTORY% -Recurse -force
+
+powershell docker run --rm -v %OUTPUT_DIRECTORY%:/out -v %INTERFACE_DIRECTORY%:/specs openapitools/openapi-generator-cli generate -i specs/%INTERFACE_FILE% -o /out -g csharp-netcore --additional-properties targetFramework=net5.0,packageName=%PACKAGE_NAME%,packageVersion=%INTERFACE_VERSION%,enumValueSuffix=,removeEnumValuePrefix=false
+
