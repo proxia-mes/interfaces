cbb408e157511dcc1653501cc749fad905a714f3
FBuchner <fbuchner@proxia.com>
2021-12-14 16:25:05 +0100

    Adapt ComuDb package

--- topics/comu-db_TRS_RES_change/v1/local-gen/GenerateNetClient.cmd
+++ topics/comu-db_TRS_RES_change/v1/local-gen/GenerateNetClient.cmd
@@ -3,7 +3,7 @@ REM https://github.com/OpenAPITools/openapi-generator
 SET OUTPUT_DIRECTORY=${PWD}/out
 SET INTERFACE_DIRECTORY=${PWD}/..
 SET INTERFACE_FILE=proxia-mes_comu-db_TRS_RES_change-event_v1.yaml
-SET PACKAGE_NAME=PAG.Mes.MonitoringDashboard.ComuDbTrsResChangeEvent
+SET PACKAGE_NAME=PAG.Mes.ComuDb.TrsResChangeEvent
 SET INTERFACE_VERSION=1.0.0
 
 powershell Remove-Item %OUTPUT_DIRECTORY% -Recurse -force
