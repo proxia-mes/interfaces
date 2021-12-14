b204f5e15f19983328a9e3fac9d9c65411734226
FBuchner <fbuchner@proxia.com>
2021-12-14 16:19:05 +0100

    Adapt ComuDb wording

--- monitoring-dashboard/comu-db-service/v1/local-gen/net-controller/GenerateNetController.cmd
+++ monitoring-dashboard/comu-db-service/v1/local-gen/net-controller/GenerateNetController.cmd
@@ -3,7 +3,7 @@ REM https://github.com/OpenAPITools/openapi-generator
 SET OUTPUT_DIRECTORY=${PWD}/out
 SET INTERFACE_DIRECTORY=${PWD}/../..
 SET INTERFACE_FILE=proxia-mes_monitoring-dashboard_comu-db-service_v1.yaml
-SET PACKAGE_NAME=PAG.Mes.MonitoringDashboard.ComuDBService.ControllerStub
+SET PACKAGE_NAME=PAG.Mes.MonitoringDashboard.ComuDbService.ControllerStub
 SET INTERFACE_VERSION=1.0.0
 
 powershell Remove-Item %OUTPUT_DIRECTORY% -Recurse -force
