c274663edf53c9a70485dc67f8e4a3c9e25b887f
FBuchner <fbuchner@proxia.com>
2021-12-15 07:19:08 +0100

    Unifying Comu-Db Events

--- topics/comu-db_change-events/v1/local-gen/GenerateNetClient.cmd
+++ topics/comu-db_change-events/v1/local-gen/GenerateNetClient.cmd
@@ -2,8 +2,8 @@ REM https://github.com/OpenAPITools/openapi-generator
 
 SET OUTPUT_DIRECTORY=${PWD}/out
 SET INTERFACE_DIRECTORY=${PWD}/..
-SET INTERFACE_FILE=proxia-mes_comu-db_TRS_RES_change-event_v1.yaml
-SET PACKAGE_NAME=PAG.Mes.ComuDb.TrsResChangeEvent
+SET INTERFACE_FILE=proxia-mes_comu-db_change-events_v1.yaml
+SET PACKAGE_NAME=PAG.Mes.ComuDb.ChangeEvents
 SET INTERFACE_VERSION=1.0.0
 
 powershell Remove-Item %OUTPUT_DIRECTORY% -Recurse -force
