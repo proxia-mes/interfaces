516e78764fc73daf2bff4e32e1b8436ef091d42e
FBuchner <fbuchner@proxia.com>
2022-02-08 15:45:27 +0100

    dashboard-notification-service: Initial Add

---
+++ monitoring-dashboard/notification-service/v1/local-gen/net-controller/GenerateNetController.cmd
@@ -0,0 +1,12 @@
+REM https://github.com/OpenAPITools/openapi-generator
+
+SET OUTPUT_DIRECTORY=${PWD}/out
+SET INTERFACE_DIRECTORY=${PWD}/../..
+SET INTERFACE_FILE=proxia-mes_monitoring-dashboard_notification-service_v1.yaml
+SET PACKAGE_NAME=PAG.Mes.MonitoringDashboard.NotificationService.ControllerStub
+SET INTERFACE_VERSION=1.0.0
+
+powershell Remove-Item %OUTPUT_DIRECTORY% -Recurse -force
+
+powershell docker run --rm -v %OUTPUT_DIRECTORY%:/out -v %INTERFACE_DIRECTORY%:/specs openapitools/openapi-generator-cli:v5.3.0 generate -i specs/%INTERFACE_FILE% -o /out -g aspnetcore --additional-properties aspnetCoreVersion=5.0,newtonsoftVersion=5.0.10,buildTarget=library,operationModifier=abstract,packageName=%PACKAGE_NAME%,packageVersion=%INTERFACE_VERSION%
+
