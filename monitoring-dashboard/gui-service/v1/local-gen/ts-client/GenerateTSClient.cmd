26c31cf1c30daf7c5b7063c5597cc161de2d56ec
FBuchner <fbuchner@proxia.com>
2022-08-24 11:37:23 +0200

    gui-service: Update to Angular 14

--- monitoring-dashboard/gui-service/v1/local-gen/ts-client/GenerateTSClient.cmd
+++ monitoring-dashboard/gui-service/v1/local-gen/ts-client/GenerateTSClient.cmd
@@ -8,5 +8,5 @@ SET PACKAGE_NAME=@proxia-mes/monitoring-dashboard-gui-service-api
 
 powershell Remove-Item %OUTPUT_DIRECTORY% -Recurse -Force -ErrorAction SilentlyContinue
 REM generate
-powershell docker run --rm -v %OUTPUT_DIRECTORY%:/out -v %INTERFACE_DIRECTORY%:/specs openapitools/openapi-generator-cli:v6.0.0 generate -i specs/%INTERFACE_FILE% -o /out -g typescript-angular --additional-properties npmName=%PACKAGE_NAME%,stringEnums=true
+powershell docker run --rm -v %OUTPUT_DIRECTORY%:/out -v %INTERFACE_DIRECTORY%:/specs openapitools/openapi-generator-cli:v6.0.1 generate -i specs/%INTERFACE_FILE% -o /out -g typescript-angular --additional-properties ngVersion=14.1.3,npmName=%PACKAGE_NAME%,stringEnums=true
 
