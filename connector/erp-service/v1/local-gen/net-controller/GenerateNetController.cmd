72fe03a9e795234344d875d8dc25b8fa5719af98
Maximilian Wandl <mwandl@proxia.com>
2024-08-07 16:50:00 +0900

    Connector ERP service: Changed back the file names of the yaml descriptions

--- connector/erp-service/v1/local-gen/net-controller/GenerateNetController.cmd
+++ connector/erp-service/v1/local-gen/net-controller/GenerateNetController.cmd
@@ -3,7 +3,7 @@ REM https://github.com/OpenAPITools/openapi-generator
 
 SET OUTPUT_DIRECTORY=${PWD}/out
 SET INTERFACE_DIRECTORY=${PWD}/../..
-SET INTERFACE_FILE=proxia-mes-erp-interface-v1.0.9.yaml
+SET INTERFACE_FILE=proxia-mes_connector_erp-service_v1.yaml
 SET PACKAGE_NAME=PAG.Mes.Connector.ERPService.ControllerStub
 SET INTERFACE_VERSION=1.0.9
 
